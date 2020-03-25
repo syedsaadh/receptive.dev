14168b7145ab60011418ef2874b5efe46d625f18
Saad Hassan <saad@propelld.com>
2020-03-26 02:59:16 +0530

    blog init

---
+++ src/components/bio.js
@@ -0,0 +1,72 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import profilePic from "../../content/assets/profile-pic.jpg"
+import { rhythm } from "../utils/typography"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
+  const { author, social } = data.site.siteMetadata
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+      }}
+    >
+      <img
+        src={profilePic}
+        alt={`Dan Abramov`}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          width: rhythm(2),
+          height: rhythm(2),
+          borderRadius: "50%",
+        }}
+      />
+      <p>
+        Personal Blog by{" "}
+        <a
+          href="https://twitter.com/syedsaad_h"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Saad Hassan
+        </a>
+        .
+        <br />I experiment and try to explain what i do.
+      </p>
+    </div>
+  )
+}
+
+export default Bio
---
+++ src/components/layout.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(0.8),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        <a
+          href="https://twitter.com/syedsaad_h"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{" "}
+        &bull;{" "}
+        <a
+          href="https://github.com/syedsaadh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
